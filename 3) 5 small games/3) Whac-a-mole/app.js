7f90652dd92b5c28fab57e8174ec076950f597c4
KarolDawidG <karoldawidg@gmail.com>
2022-10-10 19:08:06 +0200

    LOG 10: working...

--- 3) 5 small games/3) Whac-a-mole/app.js
+++ 3) 5 small games/3) Whac-a-mole/app.js
@@ -4,14 +4,47 @@ const timeLeft = document.querySelector('#time-left')
 const score = document.querySelector('#score')
 
 let result = 0
+let hitPosition
+let currentTime = 10
+let timeId = null
 
+// It is used to given object in a random place
 function randomSquare(){
     squares.forEach(square =>{
         square.classList.remove('mole')
     })
 
-let randomSquare = squares[Math.floor(Math.random()*9)] 
-randomSquare.classList.add('mole')   
+let randomSquare = squares[Math.floor(Math.random() * 9)] 
+randomSquare.classList.add('mole') 
+
+hitPosition = randomSquare.id
 }
 
-randomSquare()
+squares.forEach(square => {
+    square.addEventListener('mousedown', ()=>{
+        if (square.id == hitPosition){
+            result++
+            score.textContent = result
+            hitPosition = null
+        }
+    })
+})
+// changes in short period of time
+function moveMole(){
+    timeId = setInterval(randomSquare, 1000)
+}
+
+moveMole()
+
+function countDown(){
+currentTime--
+timeLeft.textContent = currentTime
+
+if (currentTime == 0){
+    clearInterval(countDownTimerId)
+    clearInterval(timeId)
+    alert('Game Over! Your final score is ' + result)
+}
+}
+
+let countDownTimerId = setInterval(countDown, 1000)
